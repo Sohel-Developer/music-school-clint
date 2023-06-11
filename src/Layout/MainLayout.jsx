d567e3a4a1fb396a97b153cd5e8446729c8a209f
Sohel-Developer <sohel.webdevoloper@gmail.com>
2023-06-12 01:22:09 +0600

    Footer Design done

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Manubar from '../Shared/ManuBar/Manubar';
+import Footer from '../Shared/Footer/Footer';
 
 const mainLayout = () => {
     return (
         <div>
             <Manubar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
