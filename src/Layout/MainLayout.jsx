2efa97a7f472477794b7f9516e5784cbb9444722
Sohel-Developer <sohel.webdevoloper.com>
2023-06-11 17:12:16 +0600

    Navbar Complete

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Manubar from '../Shared/ManuBar/Manubar';
 
 const mainLayout = () => {
     return (
         <div>
+            <Manubar />
             <Outlet />
         </div>
     );
