254c823febb3210d464fe4ce78b0cfda432f35e4
Sohel-Developer <sohel.webdevoloper.com>
2023-06-13 20:02:47 +0600

    Selected Class Deleted

--- src/pages/Dashbord/SelectedClasses/SelectedClasses.jsx
+++ src/pages/Dashbord/SelectedClasses/SelectedClasses.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import useAxiosSecure from '../../Hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
+import swal from 'sweetalert';
 
 const SelectedClasses = () => {
 
@@ -14,23 +16,47 @@ const SelectedClasses = () => {
             return res.data
         }
     })
+
     if (isLoading) return 'Loading...'
 
 
-    console.log(data);
+    const handelDeleteClass = (item) => {
 
+        console.log(item);
 
+        swal({
+            title: "Are you sure?",
+            text: `If Users Deleted `,
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
 
-
-
+                    fetch(`http://localhost:5000/class/delete/${item._id}`, {
+                        method: 'DELETE'
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            refetch();
+                            if (data.deletedCount > 0) {
+                                toast.success('Class Deleted Succesfully!')
+                            }
+                        })
+                } else {
+                    toast.success("Selected Class No Deleted !! it's safe!")
+                }
+            });
+    }
 
     return (
         <div className='bg-base-200 h-screen'>
             <h1 className=''>Selected Class {data?.length}</h1>
             <div >
                 {
-                    data.map(item => <tr key={item._id} className="transition-all w-1/2 mx-auto justify-between flex items-center mt-5 rounded-lg bg-gray-200 hover:shadow-lg">
-                        <td className="px-6 py-4 whitespace-nowrap">
+                    data.map(item => <div key={item._id} className="transition-all w-1/2 mx-auto justify-between flex items-center mt-5 rounded-lg bg-gray-200 hover:shadow-lg">
+                        <div className="px-6 py-4 whitespace-nowrap">
                             <div className="flex items-center">
                                 <div className="flex-shrink-0 w-10 h-10">
                                     <img
@@ -43,27 +69,27 @@ const SelectedClasses = () => {
                                     <div className="text-sm font-medium text-gray-900">{item.name} class</div>
                                 </div>
                             </div>
-                        </td>
-                        <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{item.instructorName} <br /> {item.instructorEmail}</td>
-                        <td className="px-6 py-4 whitespace-nowrap">
+                        </div>
+                        <div className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{item.instructorName} <br /> {item.instructorEmail}</div>
+                        <div className="px-6 py-4 whitespace-nowrap">
                             <span
                                 className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full"
                             >
                                 $ {item.price}
                             </span>
-                        </td>
+                        </div>
 
-                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap"><ul className="menu menu-horizontal bg-base-200 rounded-box">
+                        <div className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap"><ul className="menu menu-horizontal bg-base-200 rounded-box">
 
                             <li>
-                                <button >Delete</button>
+                                <button onClick={() => handelDeleteClass(item)} >Delete</button>
                             </li>
                             <li>
                                 <button>Pay</button>
                             </li>
                         </ul>
-                        </td>
-                    </tr>)
+                        </div>
+                    </div>)
                 }
             </div>
         </div>
